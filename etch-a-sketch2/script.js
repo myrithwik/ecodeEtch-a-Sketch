163146765903f8bd544c2fb34a6af7cf7dac75eb
Rithwik Mylavarapu <my.rithwik@berkeley.edu>
2021-04-16 15:58:04 -0700

    Added resize button

--- etch-a-sketch2/script.js
+++ etch-a-sketch2/script.js
@@ -1,13 +1,54 @@
 const container = document.getElementById("container");
+const resizeButton = document.getElementById("resize");
+
+window.addEventListener("load", setDefaultGrid(16,16));
+resizeButton.addEventListener("click", changeSize);
+
+function setDefaultGrid(size, size) {
+  makeRows(size, size);
+}
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    // cell.innerText = (c + 1);
+    cell.addEventListener("mouseover", changeColor);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(16, 16);
+
+function changeColor(e) {
+  const randomR = Math.floor(Math.random() * 256);
+  const randomG = Math.floor(Math.random() * 256);
+  const randomB = Math.floor(Math.random() * 256);
+  e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+}
+
+function changeSize() {
+  let newSize = prompt("Enter new Dimensions:");
+
+  if (newSize !== null) {
+    newSize = parseInt(newSize);
+    if (Number.isNaN(newSize)){
+      alert("Enter a Number");
+      changeSize()
+    } else if (newSize < 1 || newSize > 64) {
+      alert("Enter a number between 1 and 64");
+      changeSize()
+    } else {
+      clearGrid();
+      setDefaultGrid(newSize, newSize);
+    }
+  }
+}
+
+function clearGrid() {
+  const gridArray = Array.from(container.childNodes);
+  gridArray.forEach((element) => {
+    container.removeChild(element);
+  });
+}
+
+// setDefaultGrid();
